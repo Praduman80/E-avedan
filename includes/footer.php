9a6bb833098e0339cb65b2deea9b19b84e2d9f62
“Praduman80” <kumarparduman80@gmail.com>
2022-05-16 19:48:37 +0530

    changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
         <div class="footer-area">
-                <p>© <?php echo date("Y");?> | e-Avedan | Developed By <a href="https://kmrpraduman.me">Praduman | Ankit | Shalini </a></p>
+                <p>© <?php echo date("Y");?> | E-avedan | Developed By <a href="https://kmrpraduman.me">Praduman | Ankit | Shalini </a></p>
         </div>
 </footer>
